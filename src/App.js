66d7668e1dcc289d384426b96a8a43e61f00737d
Trite <uxio.taichi@yandex.ru>
2021-09-08 20:36:02 +0300

    profile + messages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, BrowserRouter } from "react-router-dom";
+import React from "react";
+import Profile from "./components/Profile/Profile";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Messages from "./components/Messages/Messages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+
+        <div className="wrapper row between flex-start">
+          <Sidebar />
+          <Route path="/messages" component={Messages} />
+          <Route path="/profile" component={Profile} />
+          {/* <Profile name="John" lastname="Doe" img="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"/> */}
+          {/* <Dialogs /> */}
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
