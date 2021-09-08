6f60daa419697bb7534ea6095143ca1ba53a0377
Trite <uxio.taichi@yandex.ru>
2021-09-09 00:53:10 +0300

    data moved to index

--- src/App.js
+++ src/App.js
@@ -6,18 +6,23 @@ import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Messages from "./components/Messages/Messages";
 
-function App() {
+function App(props) {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
-
         <div className="wrapper row between flex-start">
-          <Sidebar />
-          <Route path="/messages" component={Messages} />
-          <Route path="/profile" component={Profile} />
-          {/* <Profile name="John" lastname="Doe" img="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"/> */}
-          {/* <Dialogs /> */}
+          <Sidebar nav={props.nav}/>
+          <Route
+            path="/messages"
+            render={() => <Messages messages={props.messages} dialogs={props.dialogs}/>}
+          />
+          <Route
+            path="/profile"
+            render={() => (
+              <Profile name={props.profile.name} img={props.profile.img} posts={props.posts}/>
+            )}
+          />
         </div>
       </div>
     </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import NavItem from "./components/Sidebar/NavItem";
+import Message from "./components/Messages/Message";
+import Post from "./components/Post/Post";
+import Dialog from './components/Dialogs/Dialog'
+
+let messageData = [
+  { id: "1", text: "Hiii" },
+  { id: "2", text: "Hey, how are things?" },
+  { id: "3", text: "Could've been better" },
+];
+
+let profileData = {
+  name: "John Doe",
+  img: "https://bit.ly/3BUJXdi",
+};
+
+let navData = [
+  { to: "profile", text: "My Profile" },
+  { to: "feed", text: "Feed" },
+  { to: "messages", text: "Mesages" },
+  { to: "media", text: "Media" },
+  { to: "friends", text: "Friends" },
+];
+
+let postsData = [
+  {id: "1", name: "John Doe", img: "https://bit.ly/3BUJXdi", text: "This is my firts post!", date: "08.09.2021", likes: "23", reposts: "3"},
+  
+]
+let dialogsData = [
+  {id: "1", title: "Geralt", img: "https://bit.ly/3tDkkLf"},
+  {id: "2", title: "Triss", img: "https://bit.ly/3yWQ8vx"},
+  {id: "3", title: "Ciri", img: "https://bit.ly/3BUfXhz"},
+  {id: "4", title: "Dandelion", img: "https://bit.ly/38P2dIz"},
+];
+
+let dialogs = dialogsData.map( (el) =>{
+  return(<Dialog id={el.id} title={el.title} img={el.img}/>)
+});
+
+
+let nav = navData.map((item) => <NavItem to={item.to} text={item.text} />);
+let messages = messageData.map((message) => <Message text={message.text} />);
+let posts = postsData.map(post => <Post name={post.name} img={post.img} text={post.text} date={post.date} likes={post.likes} reposts={post.reposts}/>)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App messages={messages} profile={profileData} nav={nav} posts={posts} dialogs={dialogs}/>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
