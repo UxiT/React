068d7b3da75cdc8d7278b3f2b52f91da8f978661
UxiT <uxio.taichi@yandex.ru>
2021-10-14 23:08:45 +0300

    'v0.56'

--- src/components/Users/UsersC.jsx
+++ src/components/Users/UsersC.jsx
@@ -1,6 +1,42 @@
+import react from "react";
 import { connect } from "react-redux";
-import UsersAPIComponent from "./UsersAPIComponent";
 import { followAC, unfollowAC, setUsersAc, setPageAC, setTotalCountAC } from "../../redux/usersReducer";
+import Users from "./Users";
+import * as axios from "axios";
+
+class UsersContainer extends react.Component {
+  componentDidMount() {
+    if (this.props.users.length === 0) {
+      axios
+        .get(`http://127.0.0.1:8000/api/v1/users/list?page=${this.props.currentPage}&size=${this.props.pageSize}`)
+        .then((response) => {
+          this.props.setUsers(response.data.results);
+          this.props.setTotalCount(response.data.count);
+        });
+    }
+  }
+
+  setPage = (page) => {
+    this.props.setPage(page);
+    axios.get(`http://127.0.0.1:8000/api/v1/users/list?page=${page}&size=${this.props.pageSize}`).then((response) => {
+      this.props.setUsers(response.data.results);
+    });
+  };
+
+  render() {
+    return (
+      <Users
+        totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        currentPage={this.props.currentPage}
+        users={this.props.users}
+        setPage={this.setPage}
+        follow={this.props.follow}
+        unfollow={this.props.unfollow}
+      />
+    );
+  }
+}
 
 let mapStateToProps = (state) => {
   return {
@@ -33,6 +69,6 @@ let mapDispatchToProps = (dispatch) => {
   };
 };
 
-const UsersC = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
+const UsersC = connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
 
 export default UsersC;
